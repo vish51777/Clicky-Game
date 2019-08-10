dae9168919b528e3c6f8f0e67bdf097f23253f22
Vishaal <vishaal@handimaps.com>
2019-08-10 07:55:47 -0700

    Updates

---
+++ precache-manifest.ebce852e59af8081abbe2bb705b2c6fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b8499d2d23c7bf780d580de056f6223",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "abcb67c47ee02f0db1cf",
+    "url": "/Clicky-Game/static/css/main.5797c640.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "abcb67c47ee02f0db1cf",
+    "url": "/Clicky-Game/static/js/main.74ea3114.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
